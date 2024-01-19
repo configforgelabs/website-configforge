757681ab3dc81e9b7a825591ce3730026947c23f
Tobias Schuele <69237751+JustInnIO@users.noreply.github.com>
2024-01-19 14:27:19 +0100

    Add defaults

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
 import DocsListPage from "../components/DocsListPage";
+import Heading from '@theme/Heading';
+import styles from './index.module.css';
 
 export default function Projects() {
     return (
