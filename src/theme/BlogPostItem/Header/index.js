6efdeb12a3df36691689ac03ff5525b027376e63
haisontop <haisontop@gmail.com>
2024-03-13 12:26:27 -0700

    update blog and kb pages

---
+++ src/theme/BlogPostItem/Header/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import BlogPostItemHeaderTitle from "@theme/BlogPostItem/Header/Title";
+import BlogPostItemHeaderInfo from "@theme/BlogPostItem/Header/Info";
+import BlogPostItemHeaderAuthors from "@theme/BlogPostItem/Header/Authors";
+import Paper from "@site/src/components/Icons/Paper";
+import { useBlogPost } from "@docusaurus/theme-common/internal";
+export default function BlogPostItemHeader() {
+  const { metadata } = useBlogPost();
+  const { tags } = metadata;
+  const tagsExists = tags.length > 0;
+
+  return (
+    <header>
+      {tagsExists && (
+        <div className="flex flex-wrap gap-1 justify-center mb-1.5">
+          {tags.map((tag) => (
+            <span className="bg-primary-100 text-primary-800 font-medium me-2 px-2.5 py-0.5 rounded inline-flex items-center justify-center gap-x-1">
+              <Paper className="w-3 h-3" />
+              <span className="text-xs">{tag.label}</span>
+            </span>
+          ))}
+        </div>
+      )}
+      <BlogPostItemHeaderTitle />
+      <div className="flex items-center justify-center gap-x-4 mb-8">
+        <BlogPostItemHeaderAuthors />
+        <BlogPostItemHeaderInfo />
+      </div>
+    </header>
+  );
+}
