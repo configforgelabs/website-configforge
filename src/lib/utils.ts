c6bb36bb6c025f4e95b49993d80d14d52612a350
haisontop <haisontop@gmail.com>
2024-07-05 07:36:17 -0700

    fix quotation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,17 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function removeQuotations(stringValue: string) {
+  let stagingValue = stringValue
+
+  if(stringValue.startsWith("“")){
+    stagingValue = stagingValue.substring(1)
+  }
+
+  if(stringValue.endsWith("”")) {
+    stagingValue = stagingValue.slice(0, -1);
+  }
+
+  return stagingValue
+}
